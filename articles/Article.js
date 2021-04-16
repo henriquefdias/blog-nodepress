136782985a269e4221a0d04f131f8209ea1eb97d
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-04-15 21:55:45 -0300

    Adição de relacionamento
    Adição de relacionamento entre os models Article e Category

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/database");
+const Category = require("../categories/Category");
 
 const Article = connection.define('articles', {
     title:{
@@ -15,5 +16,8 @@ const Article = connection.define('articles', {
         allowNull: false
     }
 })
+
+Category.hasMany(Article); // UMA Categoria pode possuir VÁRIOS Artigos
+Article.belongsTo(Category); // UM Artigo pertence à UMA Categoria
 
 module.exports = Article;
