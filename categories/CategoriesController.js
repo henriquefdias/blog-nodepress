4c7e9579b9c5722c2ad6bad9f6a07eaa4fcd7ec8
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-04-23 17:03:30 -0300

    Inserção de categorias no banco de dados

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
+})
+
+router.post("/categories/save", (req, res) => {
+    let title = req.body.title;
+    if(title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        })
+    } else {
+        window.alert("Insira um nome válido para a categoria.")
+        res.redirect("/admin/categories/new");
+    }
 })
 
 module.exports = router;
