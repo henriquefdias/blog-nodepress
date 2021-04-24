603790093bc2e8d3eeb1ba52139fb0604cd961c6
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-04-24 00:29:20 -0300

    Adição da tela de listagem de Categorias
    Adição da tela de listagem de Categorias, além da possibilidade de exclusão de categoria.

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -9,7 +9,7 @@ router.get("/admin/categories/new", (req, res) => {
 
 router.post("/categories/save", (req, res) => {
     let title = req.body.title;
-    if(title != undefined) {
+    if (title != undefined) {
         Category.create({
             title: title,
             slug: slugify(title)
@@ -21,5 +21,30 @@ router.post("/categories/save", (req, res) => {
         res.redirect("/admin/categories/new");
     }
 })
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", { categories: categories });
+    })
+});
+
+router.post("/categories/delete", (req, res) => {
+    let id = req.body.id
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            })
+        } else {
+            res.redirect("/admin/categories");
+        }
+    } else {
+        res.redirect("/admin/categories");
+    }
+});
 
 module.exports = router;
