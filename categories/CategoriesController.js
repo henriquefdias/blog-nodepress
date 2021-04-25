55836fef84c1d533c9f15187e841a3d107c3f293
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-04-25 13:57:08 -0300

    Ínicio da adição de edição de categoria.

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect("/");
+            res.redirect("/admin/categories");
         })
     } else {
         window.alert("Insira um nome válido para a categoria.")
@@ -45,6 +45,22 @@ router.post("/categories/delete", (req, res) => {
     } else {
         res.redirect("/admin/categories");
     }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    let id = req.params.id
+    if(isNaN(id)) {
+        res.redirect("/admin/categories");
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined) {
+            res.render("admin/categories/edit", {category: category})
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    })
 });
 
 module.exports = router;
