311c83a08d0ddbd7c0209b616f5897ac5cbf4d0d
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-05-03 17:13:17 -0300

    Início do sistema de login

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const connection = require("./database/database");
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
+const usersController = require("./user/UsersController");
 
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const User = require("./user/User");
 
 // View engine
 app.set('view engine', 'ejs');
@@ -29,6 +31,7 @@ connection
 
 app.use("/", categoriesController);
 app.use("/", articlesController);
+app.use("/", usersController);
 
 app.get("/", (req, res) => {
     Article.findAll({
